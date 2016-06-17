1ba52dccc545a0a243da655dac9a509edecd98f6
daviinacio <aazz6850@gmail.com>
2016-06-17 10:27:37 -0300

    Revert "Arruma 3"
    This reverts commit 81c71a86deabcc483997adda0a258c5e8db175c2.

---
+++ Joao marcos/Teste_SideScroller/Teste_SideScroller/MainWind.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Teste_SideScroller
+{
+    partial class MainWind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(784, 462);
+            this.canvas.TabIndex = 0;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // MainWind
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.canvas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainWind";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste Side Scroller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWind_FormClosing);
+            this.Load += new System.EventHandler(this.MainWind_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainWind_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainWind_KeyUp);
+            this.Resize += new System.EventHandler(this.MainWind_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+    }
+}
+
